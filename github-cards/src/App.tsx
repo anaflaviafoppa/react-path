193229dd79f2123ce0d14505356206b3ca16ee15
Ana Foppa <Ana.Foppa@ctw.bmwgroup.com>
2022-11-13 20:16:48 +0000

    First commit cards

---
+++ github-cards/src/App.tsx
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+export default class MyComponent extends Component {
+    render() {
+        return (
+            <div>
+                <h1>The GitHub Cards App</h1>
+            </div>
+        );
+    }
+}
+
