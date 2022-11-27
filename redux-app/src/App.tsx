f9c9404ebab84a6a6d4e9c3a42e9c40704196661
Ana Foppa <Ana.Foppa@ctw.bmwgroup.com>
2022-11-27 15:58:33 +0000

    Start route redux app

--- redux-app/src/App.tsx
+++ redux-app/src/App.tsx
@@ -1,25 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import HomePage from './views/pages/home-page/HomePage';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import AboutPage from './views/pages/about-page/AboutPage';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <BrowserRouter>
+          <Routes>
+              <Route path="/" element={<HomePage></HomePage>}></Route>
+              <Route path="/about" element={<AboutPage></AboutPage>}></Route>
+          </Routes>
+      </BrowserRouter>
   );
 }
 
